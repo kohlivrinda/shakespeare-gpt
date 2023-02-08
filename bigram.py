1c27b0c4d4b8587b0bd4737bbfbde31e35c46ef5
Vrinda Kohli <vrindakohliofficial@gmail.com>
2023-02-09 01:19:37 +0530

    needs tuning and dropouts.

--- bigram.py
+++ bigram.py
@@ -7,14 +7,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 #hyperparameters
-block_size = 8
-batch_size=32
+block_size = 256
+batch_size=64
 max_iters=5000
 eval_interval=300
-learning_rate=1e-3
+learning_rate=3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters=200
-n_embd=32
+n_embd=384
+n_layer=6
+n_head=6
+dropout=0.2
 #------------------
 
 torch.manual_seed(1337)
@@ -77,6 +80,7 @@ class Head(nn.Module):
     self.query=nn.Linear(n_embd, head_size, bias=False)
     self.value =nn.Linear(n_embd, head_size, bias=False)
     self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    self.dropout=nn.Dropout(dropout)
     
   def forward(self, x):
     B,T,C=x.shape 
@@ -88,7 +92,8 @@ class Head(nn.Module):
     wei=q@k.transpose(-2, -1) *C**-0.5 #(B,T,C) @ (B,C,T) --> (B,T,T) 
     #formula used above, the root divide done to keep things scaled
     wei=wei.masked_fill(self.tril[:T, :T]==0, float('-inf')) # (B,T,T)
-    wei=F.softmax(wei, dim=-1)
+    wei=F.softmax(wei, dim=-1) #BTT
+    wei=self.dropout(wei)
     
     #weighted aggregation of the vals
     v=self.value(x)
@@ -100,21 +105,67 @@ class MultiHeadAttention(nn.Module):
   def __init__(self, num_heads, head_size):
     super().__init__()
     self.heads=nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    self.proj=nn.Linear(n_embd, n_embd)
+    self.dropout=nn.Dropout(dropout)
+  def forward(self, x):
+    out=torch.cat([h(x) for h in self.heads], dim=-1) #dim -1 means Channel dimension
+    out=self.proj(out)
+    return out
+  
+  
+class FeedForward(nn.Module):
+  '''a simple linear layer followed by non-linearity'''
+  
+  def __init__(self, n_embd):
+    super().__init__()
+    self.net=nn.Sequential(
+      nn.Linear(n_embd, 4*n_embd),
+      nn.ReLU(),
+      nn.Linear(4*n_embd, n_embd), #projection layer
+      nn.Dropout(dropout)
+    )
     
   def forward(self, x):
-    return torch.cat([h(x) for h in self.heads], dim=-1) #dim -1 means Channel dimension
+    return self.net(x)
   
+class Block(nn.Module):
   
-#simple bigram model
-class BigramLanguageModel(nn.Module):
-
-  def __init__(self, vocab_size):
+  '''Transformer block: communication followed by comutation'''
+  
+  def __init__(self, n_embd, n_head):
     super().__init__()
+    head_size=n_embd//n_head
+    self.sa=MultiHeadAttention(n_head, head_size)
+    self.ffwd=FeedForward(n_embd)
+    self.ln1=nn.LayerNorm(n_embd)
+    self.ln2=nn.LayerNorm(n_embd)
+    
+  def forward(self, x):
+    x=x+self.sa(self.ln1(x))
+    x=x+self.ffwd(self.ln2(x))
+    return x
+  
+#simple bigram model eventually converted into a GPT model
+class GPTLanguageModel(nn.Module):
+
+  def __init__(self): 
+    super().__init__()
+    #each token directly reads off the logits for the next token from a lookup table
     self.token_embedding_table=nn.Embedding(vocab_size, n_embd) #create a CxC embedding table
     self.position_embedding_table=nn.Embedding(block_size, n_embd)
-    self.sa_heads=MultiHeadAttention(4,n_embd//4)
+    self.blocks=nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+    self.ln_f=nn.LayerNorm(n_embd)
     self.lm_head=nn.Linear(n_embd, vocab_size) #language model head 
+    self.apply(self._init_weights)
     
+  def _init_weights(self, module):
+    if isinstance(module, nn.Linear):
+      torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+      if module.bias is not None:
+        torch.nn.init.zeros_(module.bias)
+      elif isinstance(module, nn.Embedding):
+        torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        
   def forward(self, idx, targets=None):
     B,T=idx.shape
     
@@ -122,7 +173,7 @@ class BigramLanguageModel(nn.Module):
     tok_emb=self.token_embedding_table(idx) #(B,T,C tensor (batch x time x channel, here C is the vocab_size ))
     pos_emb=self.position_embedding_table(torch.arange(T, device=device)) #(T,C)
     x=tok_emb+pos_emb  
-    x=self.sa_heads(x) #applying one head of self attention
+    x=self.blocks(x) 
     logits = self.lm_head(x) #(B, T, vocab_size )
     if targets is None:
       loss = None
@@ -151,7 +202,7 @@ class BigramLanguageModel(nn.Module):
       idx=torch.cat((idx, idx_next), dim=1) #(B, T+1)
     return idx
     
-model=BigramLanguageModel(vocab_size)
+model=GPTLanguageModel()
 m=model.to(device)
 
 #create a PyTorch optimizer
@@ -176,4 +227,4 @@ for iter in range(max_iters):
 #generating from the model
 context=torch.zeros((1,1), dtype=torch.long, device=device)
 print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-
+open('output.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
